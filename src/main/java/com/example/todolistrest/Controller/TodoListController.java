d17334fc255236595a897126159407bebe337e4d
Oralita <oat431@gmail.com>
2021-05-15 18:11:06 +0700

    [ 🍉 add ] : TodoList and TodoTask Controller

---
+++ src/main/java/com/example/todolistrest/Controller/TodoListController.java
@@ -0,0 +1,49 @@
+package com.example.todolistrest.Controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import com.example.todolistrest.Model.TodoList;
+import com.example.todolistrest.Config.ResourceNotFound;
+import com.example.todolistrest.Repository.TodoListRepo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/todo")
+public class TodoListController {
+    @Autowired
+    private TodoListRepo todoListRepo;
+
+    @GetMapping("/todolist")
+    public List<TodoList> getAllTodoList(){
+        return todoListRepo.allTodoList();
+    }
+
+    @PostMapping("/todolist")
+    public int addNewTodoList(@Valid @RequestBody TodoList newTodoList){
+        return todoListRepo.addTodoList(newTodoList);
+    }
+
+    @PutMapping("/todolist/{id}")
+    public ResponseEntity<Integer> updateTodoList(@PathVariable(value="id")Long todo_no, @RequestBody TodoList updateData) throws ResourceNotFound {
+        TodoList updateTodoList = todoListRepo.TodoListDetailById(todo_no).orElseThrow(()->new ResourceNotFound("don't have this todo please check again"));
+        updateTodoList.setTitle(updateData.getTitle());
+        updateTodoList.setDescription(updateData.getDescription());
+        return ResponseEntity.ok(todoListRepo.updateTodoList(updateTodoList));
+    }
+
+    @DeleteMapping("/todolist/{id}")
+    public Map<String,Boolean> deleteTodoList(@PathVariable(value="id")Long todo_no) throws ResourceNotFound{
+        TodoList deleteTodoList = todoListRepo.TodoListDetailById(todo_no).orElseThrow(()->new ResourceNotFound("Not have this todo list yet"));
+        todoListRepo.deleteTodoList(deleteTodoList);
+        Map<String,Boolean> res = new HashMap<String,Boolean>();
+        res.put("Deleted",Boolean.TRUE);
+        return res;
+    }
+}
---
+++ src/main/java/com/example/todolistrest/Controller/TodoTaskController.java
@@ -0,0 +1,56 @@
+package com.example.todolistrest.Controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import com.example.todolistrest.Model.TodoTask;
+import com.example.todolistrest.Repository.TodoTaskRepo;
+import com.example.todolistrest.Config.ResourceNotFound;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/todo")
+public class TodoTaskController {
+    @Autowired
+    private TodoTaskRepo todoTaskRepo;
+
+    @GetMapping("/{todo_no}/allTasks")
+    public List<TodoTask> getAllTask(@PathVariable(value="todo_no")Long todo_no){
+        return todoTaskRepo.getAllTaskByTodoList(todo_no);
+    }
+
+    @PostMapping("/{todo_no}/addTask")
+    public int addNewTask(@Valid @RequestBody TodoTask newTask,@PathVariable(value="todo_no")Long todo_no){
+        return todoTaskRepo.addTodoTask(newTask,todo_no);
+    }
+
+    @PutMapping("/{todo_no}/updateTask/{task_no}")
+    public ResponseEntity<Integer> updateTask(@Valid @RequestBody TodoTask updatedTask,@PathVariable(value="todo_no")Long todo_no,@PathVariable(value="task_no")Long task_no) throws ResourceNotFound{
+        TodoTask prepareToUpdate = todoTaskRepo.findTaskById(task_no).orElseThrow(()->new ResourceNotFound("not found"));
+        prepareToUpdate.setTask(updatedTask.getTask());
+        prepareToUpdate.setDescription(updatedTask.getDescription());
+        return ResponseEntity.ok(todoTaskRepo.update(prepareToUpdate,todo_no));
+    }
+
+    @PutMapping("/{todo_no}/updateStatus/{task_no}/{status}")
+    public ResponseEntity<Integer> updateStatus(@Valid @RequestBody TodoTask updatedTask,@PathVariable(value="todo_no")Long todo_no,@PathVariable(value="task_no")Long task_no,@PathVariable(value="status")Boolean status) throws ResourceNotFound{
+        TodoTask prepareToUpdate = todoTaskRepo.findTaskById(task_no).orElseThrow(()->new ResourceNotFound("not found"));
+        prepareToUpdate.setStatus(updatedTask.getStatus());
+        return ResponseEntity.ok(todoTaskRepo.updateStatus(prepareToUpdate,status,todo_no));
+    }
+
+    @DeleteMapping("/{todo_no}/delteTask/{task_no}")
+    public Map<String,Boolean> deleteTask(@PathVariable(value="todo_no")Long todo_no,@PathVariable(value="task_no")Long task_no) throws ResourceNotFound {
+        TodoTask prepareToDelete = todoTaskRepo.findTaskById(task_no).orElseThrow(()->new ResourceNotFound("not found"));
+        todoTaskRepo.deleteTask(prepareToDelete);
+        Map<String,Boolean> res = new HashMap<>();
+        res.put("Deleted",Boolean.TRUE);
+        return res;
+    }
+}
