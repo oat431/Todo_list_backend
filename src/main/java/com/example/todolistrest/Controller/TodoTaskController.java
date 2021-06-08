a81c83561d700f364aabb84fc4fa7ca641143d51
Oralita <oat431@gmail.com>
2021-06-08 10:36:42 +0700

    [ 🍧 add ] : cross origin to access from js

--- src/main/java/com/example/todolistrest/Controller/TodoTaskController.java
+++ src/main/java/com/example/todolistrest/Controller/TodoTaskController.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(maxAge = 3600)
 @RestController
 @RequestMapping("/api/todo")
 public class TodoTaskController {
